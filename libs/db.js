d0abd89c8fcd969336c4508a1d3d9ba28f25e9e9
AlexUA <alexua89@gmail.com>
2015-11-08 21:01:14 +0200

    db connected listener

--- libs/db.js
+++ libs/db.js
@@ -1,6 +1,11 @@
 var database = require('mongoose');
 var config = require('configuration');
 
-database.connect(config.get('mongoose:uri'), config.get("mongoose:options"));
+var db = database.connect(config.get('mongoose:uri'), config.get("mongoose:options")).connection;
+
+//db.on('error', function(err) { console.log(err.message); });
+db.once('open', function() {
+    console.log("mongodb connection open");
+});
 
 module.exports = database;
