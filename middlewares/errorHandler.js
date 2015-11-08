9e9865389e772f9154c6e232f2401b7c39200ae6
AlexUA <alexua89@gmail.com>
2015-11-09 00:26:51 +0200

    disable error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,19 +1,19 @@
-var logger =  require('libs/log');
-
-module.exports.init =  function init (app) {
-
-    app.use(function errorCallBack(err, req, res, next) {
-        logger.crit('error on request %d %s %s', process.domain.id, req.method, req.url);
-        logger.crit(err.stack);
-        if(!!err && !res.headersSent){
-            res.send(500, "Something bad happened. :(");
-        }
-        if(err.domain) {
-             //TODO do something with this this error
-        }
-    });
-
-};
+//var logger =  require('libs/log');
+//
+//module.exports.init =  function init (app) {
+//
+//    app.use(function errorCallBack(err, req, res, next) {
+//        logger.crit('error on request %d %s %s', process.domain.id, req.method, req.url);
+//        logger.crit(err.stack);
+//        if(!!err && !res.headersSent){
+//            res.send(500, "Something bad happened. :(");
+//        }
+//        if(err.domain) {
+//             //TODO do something with this this error
+//        }
+//    });
+//
+//};
 
 
 
